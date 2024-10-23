b2ed1d8eb26119d8a461ac732f434f7fb6f9234e
nuttycream <issanutty@gmail.com>
2024-10-22 22:44:20 -0700

    feat: complete implementation of week-2 milestone web-server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,31 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import httpErrors from "http-errors";
+import morgan from "morgan";
+import * as path from "path";
+
 import rootRoutes from "./routes/root"
+import { timeMiddleware } from "./middleware/time";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// middleware
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(timeMiddleware);
+app.use(express.static(
+    path.join(process.cwd(), "src", "public"))
+);
+app.use(cookieParser());
+app.set("views", path.join(process.cwd(), "src", "server", "views"));
+app.set("view engine", "ejs");
+
 app.get("/", rootRoutes);
+
+app.use((_req: express.Request, _res: express.Response, next: express.NextFunction) => {
+    next(httpErrors(404));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- src/server/routes/root.ts
+++ src/server/routes/root.ts
@@ -3,8 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (_req: express.Request, res: express.Response) => {
-    res.status(200)
-    res.json({ title: "coding block's site" });
+    res.render("root", { name: "codingblocks site"});
 });
 
 export default router;
