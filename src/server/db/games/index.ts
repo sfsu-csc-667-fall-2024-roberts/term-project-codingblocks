f2e3da0c11a8a31832d4833adf4cef711894df7d
nuttycream <issanutty@gmail.com>
2024-12-07 20:49:32 -0800

    feat: impl additional game lobby sql ops

--- src/server/db/games/index.ts
+++ src/server/db/games/index.ts
@@ -1,28 +1,69 @@
 import db from "../connection";
 import {
-  CREATE_GAME,
-  ADD_PLAYER,
-  GET_PLAYER_HAND,
-  DRAW_CARD,
-  UPDATE_GAME_STATE,
+    CREATE_GAME,
+    ADD_PLAYER,
+    GET_PLAYER_HAND,
+    DRAW_CARD,
+    UPDATE_GAME_STATE,
+    ALL_PLAYER_DATA,
 } from "./sql";
 
 export const createGame = async (): Promise<{ id: number }> => {
-  return await db.one(CREATE_GAME);
+    return await db.one(CREATE_GAME);
 };
 
-export const joinGame = async (gameId: number, userId: number): Promise<{ game_id: number; user_id: number; seat: number }> => {
-  return await db.one(ADD_PLAYER, [gameId, userId]);
+export const get = async (gameId: number, playerId: number) => {
+    const currentSeat = await db.one(
+        "SELECT current_seat FROM games WHERE id=$1",
+        gameId,
+    );
+    const players = await getPlayers(gameId);
+    const playerHand = await db.any(GET_PLAYER_HAND, [playerId, gameId, 0, 8]);
+
+    return {
+        currentSeat,
+        players,
+        playerHand,
+    };
+};
+
+export const getPlayers = async (
+    gameId: number,
+): Promise<
+    {
+        gravatar: string;
+        id: number;
+        is_current: boolean;
+        last_draw_turn: number;
+        pile_1: number[];
+        pile_2: number[];
+        pile_3: number[];
+        pile_4: number[];
+        play_pile_top: number;
+        play_pile_top_id: number;
+        play_pile_count: number;
+        seat: number;
+        username: string;
+    }[]
+> => {
+    return await db.any(ALL_PLAYER_DATA, [gameId]);
+};
+
+export const joinGame = async (
+    gameId: number,
+    userId: number,
+): Promise<{ game_id: number; user_id: number; seat: number }> => {
+    return await db.one(ADD_PLAYER, [gameId, userId]);
 };
 
 export const getPlayerHand = async (gameId: number, userId: number) => {
-  return await db.any(GET_PLAYER_HAND, [gameId, userId]);
+    return await db.any(GET_PLAYER_HAND, [gameId, userId]);
 };
 
 export const drawCard = async (gameId: number, userId: number) => {
-  return await db.one(DRAW_CARD, [gameId, userId]);
+    return await db.one(DRAW_CARD, [gameId, userId]);
 };
 
 export const updateGameState = async (gameId: number, showing: boolean) => {
-  return db.none(UPDATE_GAME_STATE, [showing, gameId]);
+    return db.none(UPDATE_GAME_STATE, [showing, gameId]);
 };
--- src/server/db/games/sql.ts
+++ src/server/db/games/sql.ts
@@ -31,3 +31,63 @@ export const DRAW_CARD = `
 export const UPDATE_GAME_STATE = `
   UPDATE games SET showing = $1 WHERE id = $2;
 `;
+
+/**
+ * Player data that is viewable by all players
+ */
+const playPileTopSubquery = (field: string) => `
+SELECT ${field} 
+FROM game_cards, cards 
+WHERE game_cards.user_id=users.id 
+  AND game_cards.game_id=$1 
+  AND game_cards.card_id=cards.id 
+  AND pile=-1 
+ORDER BY position DESC LIMIT 1
+`;
+
+const PLAY_PILE_COUNT = `
+SELECT COUNT(*)
+FROM game_cards
+WHERE game_id = $1 
+  AND pile=-1
+  AND users.id = game_cards.user_id`;
+
+const discardPileSubquery = (pile: number) => `
+  SELECT cards.value FROM game_cards, cards
+  WHERE game_cards.user_id=users.id 
+    AND game_cards.game_id=$1 
+    AND game_cards.card_id=cards.id 
+    AND pile=${pile}
+  ORDER BY position DESC`;
+
+export const IS_CURRENT = `
+  SELECT games.current_seat = game_users.seat AS is_current_player
+    FROM games, game_users
+    WHERE games.id = $1
+    AND game_users.user_id = $2
+    AND game_users.game_id = games.id`;
+
+export const ALL_PLAYER_DATA = `
+SELECT 
+  users.id, 
+  users.username, 
+  users.gravatar, 
+  game_users.seat, 
+  game_users.last_draw_turn,
+  (
+    SELECT games.current_seat = game_users.seat 
+    FROM games, game_users
+    WHERE games.id = $1
+    AND game_users.user_id = users.id
+    AND game_users.game_id = games.id
+  ) AS is_current,
+  (${playPileTopSubquery("cards.value")}) AS play_pile_top,
+  (${playPileTopSubquery("cards.id")}) AS play_pile_top_id,
+  (${PLAY_PILE_COUNT})::INTEGER AS play_pile_count,
+  array(${discardPileSubquery(1)}) AS pile_1,
+  array(${discardPileSubquery(2)}) AS pile_2,
+  array(${discardPileSubquery(3)}) AS pile_3,
+  array(${discardPileSubquery(4)}) AS pile_4
+FROM users, game_users
+WHERE users.id = game_users.user_id AND game_users.game_id = $1
+`;
