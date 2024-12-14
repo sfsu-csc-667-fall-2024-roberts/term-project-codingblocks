f388d44edbe9bbef2cb4377f1ea28d87664d4c84
nuttycream <issanutty@gmail.com>
2024-12-13 17:45:02 -0800

    fix: rmd undefined type

--- src/server/config/session.ts
+++ src/server/config/session.ts
@@ -5,7 +5,7 @@ import session from "express-session";
 
 let sessionMiddleware: RequestHandler | undefined = undefined;
 
-export default (app: Express): RequestHandler | undefined => {
+export default (app: Express): RequestHandler => {
     if (sessionMiddleware === undefined) {
         sessionMiddleware = session({
             store: new (connectPgSimple(session))({
@@ -15,8 +15,10 @@ export default (app: Express): RequestHandler | undefined => {
             resave: true,
             saveUninitialized: true,
         });
+
         app.use(sessionMiddleware);
         app.use(flash());
     }
+
     return sessionMiddleware;
 };
