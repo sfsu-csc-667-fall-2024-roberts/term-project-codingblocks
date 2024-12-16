83df628896f4a04b83891da6faac1f1cca459413
nuttycream <issanutty@gmail.com>
2024-12-15 21:21:22 -0800

    feat: add game users status

---
+++ migrations/1734326237468_games-users-status.ts
@@ -0,0 +1,20 @@
+import { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+    pgm.createType("user_status", ["active", "folded", "all-in"]);
+
+    pgm.addColumn("game_users", {
+        status: {
+            type: "user_status",
+            notNull: true,
+            default: "active",
+        },
+    });
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+    pgm.dropColumn("game_users", "user_status");
+    pgm.dropType("user_status");
+}
