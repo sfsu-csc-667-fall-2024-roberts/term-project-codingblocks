c92801993a6dd909f0d563a3c9aa51fb1dd2616b
nuttycream <issanutty@gmail.com>
2024-12-17 15:30:13 -0800

    feat: impl chat route

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -2,5 +2,6 @@ export { default as auth } from "./auth";
 export { default as lobby } from "./lobby";
 export { default as games } from "./games";
 export { default as home } from "./home";
+export { default as chat } from "./chat";
 //TODO: impl tests
 //export { default as test } from "./test";
